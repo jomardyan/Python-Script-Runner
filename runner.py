10e1abffcfd2250b18f0fe82b289531eba3f4709
Jomardyan <jomardyan@outlook.com>
2025-10-24 00:25:16 +0200

    chore: bump version to 7.0.2

--- runner.py
+++ runner.py
@@ -15,7 +15,7 @@ Core Features:
 Executes a target Python script and collects comprehensive execution statistics.
 """
 
-__version__ = "7.0.1"
+__version__ = "7.0.2"
 __author__ = "Hayk Jomardyan"
 __license__ = "MIT"
 
